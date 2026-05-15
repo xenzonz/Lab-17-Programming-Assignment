888255fa23becbb69473cd9949dfbc02a9d10dc1
sam cocquyt <samcocquyt04@gmail.com>
2026-05-15 02:38:28 -0400

    docstring and typehints added

--- Lab17_xenzonz-2.py
+++ Lab17_xenzonz-2.py
@@ -12,23 +12,31 @@ v. 5/15/2026
 import requests
 import plotly.express as px
 
-def main():
-    
+def main() -> None:
+    """
+    Get JavaScript repository data from the GitHub API and create a bar chart.
+
+    This function sends a request to the GitHub API for JavaScript repositories
+    with more than 10,000 stars. It then extracts each repository's name and
+    star count and displays the data using a Plotly bar chart.
+    """    
     # Make an API call and check the response.
-    url = "https://api.github.com/search/repositories"
+    url: str = "https://api.github.com/search/repositories"
     url += "?q=language:javascript+sort:stars+stars:>10000"
 
-    headers = {"Accept": "application/vnd.github.v3+json"}
-    r = requests.get(url, headers=headers)
+    headers: dict[str, str] = {"Accept": "application/vnd.github.v3+json"}
+    r: requests.Response = requests.get(url, headers=headers)
     print(f"Status code: {r.status_code}")
 
     # Process overall results.
-    response_dict = r.json()
+    response_dict: dict = r.json()
     print(f"Complete results: {not response_dict['incomplete_results']}")
 
     # Process repository information.
-    repo_dicts = response_dict['items']
-    repo_names, stars = [], []
+    repo_dicts: list = response_dict['items']
+    repo_names: list[str] = []
+    stars: list[int] = []
+
     for repo_dict in repo_dicts:
         repo_names.append(repo_dict['name'])
         stars.append(repo_dict['stargazers_count'])
