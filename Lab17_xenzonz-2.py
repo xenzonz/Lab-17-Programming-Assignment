8a8a8697305eda57c51888eb752d2129387e99cc
sam cocquyt <samcocquyt04@gmail.com>
2026-05-15 02:15:23 -0400

    initial code added

--- Lab17_xenzonz-2.py
+++ Lab17_xenzonz-2.py
@@ -1,1 +1,18 @@
 print("hello world")
+
+"""
+Docstring for Lab17_xenzonz-2.py
+i. Lab 16: Plot GitHub Repositories for a New Language
+ii. Sam Cocquyt
+iii.  Uses the GitHub API to find the most-starred JavaScript
+    repositories on GitHub.
+
+iv. python_repos_visual.py on Chapter 17 of the textbook
+v. 5/15/2026
+"""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
