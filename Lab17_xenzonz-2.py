ef9fc2a03abeb2d095c4232cef7b0c50dab19780
sam cocquyt <samcocquyt04@gmail.com>
2026-05-15 02:30:15 -0400

    modified starter code to use javascript and changed the visuals

--- Lab17_xenzonz-2.py
+++ Lab17_xenzonz-2.py
@@ -2,7 +2,7 @@ print("hello world")
 
 """
 Docstring for Lab17_xenzonz-2.py
-i. Lab 16: Plot GitHub Repositories for a New Language
+i. Lab 17: Plot GitHub Repositories for a New Language
 ii. Sam Cocquyt
 iii.  Uses the GitHub API to find the most-starred JavaScript
     repositories on GitHub.
@@ -14,31 +14,38 @@ v. 5/15/2026
 import requests
 import plotly.express as px
 
-# Make an API call and check the response.
-url = "https://api.github.com/search/repositories"
-url += "?q=language:python+sort:stars+stars:>10000"
-
-headers = {"Accept": "application/vnd.github.v3+json"}
-r = requests.get(url, headers=headers)
-print(f"Status code: {r.status_code}")
-
-# Process overall results.
-response_dict = r.json()
-print(f"Complete results: {not response_dict['incomplete_results']}")
-
-# Process repository information.
-repo_dicts = response_dict['items']
-repo_names, stars = [], []
-for repo_dict in repo_dicts:
-    repo_names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
-
-# Make visualization.
-fig = px.bar(x=repo_names, y=stars)
-fig.show()
-
 def main():
-    pass
+    
+    # Make an API call and check the response.
+    url = "https://api.github.com/search/repositories"
+    url += "?q=language:javascript+sort:stars+stars:>10000"
+
+    headers = {"Accept": "application/vnd.github.v3+json"}
+    r = requests.get(url, headers=headers)
+    print(f"Status code: {r.status_code}")
+
+    # Process overall results.
+    response_dict = r.json()
+    print(f"Complete results: {not response_dict['incomplete_results']}")
+
+    # Process repository information.
+    repo_dicts = response_dict['items']
+    repo_names, stars = [], []
+    for repo_dict in repo_dicts:
+        repo_names.append(repo_dict['name'])
+        stars.append(repo_dict['stargazers_count'])
+
+    # Make visualization.
+    fig = px.bar(
+    x=repo_names,
+    y=stars,
+    color=stars,
+    color_continuous_scale="Viridis",
+    title="Most-Starred JavaScript Projects on GitHub",
+    template="plotly_dark"
+    )
+
+    fig.show()
 
 if __name__ == "__main__":
     main()
